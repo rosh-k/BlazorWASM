0f82caaaf380c6008101274abd086337ecb1b917
roshan.a.gurung@outlook.com <roshan.a.gurung@outlook.com>
2023-03-19 17:53:32 -0400

    Add project files.

---
+++ BlazorApp_WASM/Pages/Translate.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp_WASM.Pages
+{
+    public class Translate
+    {
+        public string GurungKey { get; set; }
+        public string EnglishValue { get; set; }
+        public string NepaliValue { get; set; }
+        public string Description { get; set; }
+    }
+}
